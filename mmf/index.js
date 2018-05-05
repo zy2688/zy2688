f0c9215d4de6bb3b0d824f5f9d97690d6e8055cc
MonsterE <zy2688@gmail.com>
2018-05-05 12:16:11 +0800

    目录折叠

--- mmf/index.js
+++ mmf/index.js
@@ -1,1 +1,10 @@
-// empty
+$(function () {
+    $('.music_list_title').on('click', function (event) {
+        $(this).next('.music_box_list').toggle(0, function () {
+            //console.log("1");
+        }, function () {
+            //console.log("0");
+        });
+    });
+});
+
